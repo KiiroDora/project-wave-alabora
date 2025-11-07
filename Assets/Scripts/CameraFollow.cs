de08d66d0759253d2b905fa9c69eb6e40ed4c803
KiiroDora <kiirodoradora@gmail.com>
2025-11-07 22:15:05 +0300

    add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform target; //target's 3D transform
+    [SerializeField] private Vector3 velocity = Vector3.zero; //reference variable for smoothdamp of camera position
+    [SerializeField] private Vector3 offset; //offset for camera
+    [SerializeField] private float smoothTime = 0.3f; //speed for linear movement
+
+
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position + offset; //set up initial position with offset
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime); //move from initial position to target's position with linear speed
+        
+        smoothPosition.z = transform.position.z; //since this is 2D don't change the z axis
+        transform.position = smoothPosition; //update position
+    }
+}
